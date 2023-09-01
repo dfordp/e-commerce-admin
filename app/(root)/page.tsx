aad650482025dca13477a191d1dd89bfb9ef8d18
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-09-01 11:37:43 +0530

    1/9 taking a break

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import { Modal } from "@/components/ui/modal";
 import { UserButton } from "@clerk/nextjs";
 
 const Setup = () => {
     return(
-      <div>
-        <UserButton afterSignOutUrl="/"/>
+      <div className="p-4">
+        <Modal title="Test" description="Test" isOpen onClose={()=>{}}>
+          Test
+        </Modal>
       </div>
     )
 }
