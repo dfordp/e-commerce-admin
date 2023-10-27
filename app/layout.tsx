129f0f2893a81615c2ce4a2bc826f28effd46c24
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-10-27 20:11:29 +0530

    added settings page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+
 import { ModalProvider } from '@/providers/modal-provider'
 import CustomToaster from '@/providers/toaster-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Admin Page',
-  description: 'Manage all your Stores Using This DashBoard Page',
+export const metadata = {
+  title: 'Dashboard',
+  description: 'E-Commerce Dashboard',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
@@ -22,9 +23,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <CustomToaster/>
-          <ModalProvider/>
-          {children}
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="system" 
+            enableSystem
+          >
+            <CustomToaster />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import { StoreModal } from "@/components/modals/StoreModal";
+import { StoreModal } from "@/components/modals/store-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
